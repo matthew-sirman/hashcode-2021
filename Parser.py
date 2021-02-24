ae498780552331d08379f632012fc1fbd5874d36
Matthew Sirman <ms2649@cam.ac.uk>
2021-02-24 17:07:20 +0000

    Basic file structure

--- Parser.py
+++ Parser.py
@@ -1,3 +1,8 @@
+class DataInput:
+    def __init__(self) -> None:
+        pass
+
+
 class Parser:
     def __init__(self, input_file: str) -> None:
         self.__in_file = input_file
@@ -5,4 +10,7 @@ class Parser:
         with open(input_file, "r") as in_file:
             for line in in_file:
                 lines.append(line)
+
+    def create_input(self) -> DataInput:
+        pass
 
